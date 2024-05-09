2374b12f5fdc88b08c4f92e721ab299fed7376f3
KyleUltimate <shiue.kyle@gmail.com>
2024-05-09 22:12:59 +0800

    chore: remove explicit option when calling in CollectionBlock

--- src/Collections.rs
+++ src/Collections.rs
@@ -57,7 +57,7 @@ pub fn Collections() -> Element {
             div { class: "grid grid-flow-row grid-cols-[repeat(auto-fill,280px)] gap-[20px]",
                 for collection in collections_iterator {
                     CollectionBlock {
-                        collection: Some(collection),
+                        collection,
                         extended_class: "rounded-[20px]",
                         picture: COLLECTION_PIC
                     }
